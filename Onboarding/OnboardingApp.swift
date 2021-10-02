b78b9a9601bdabcd3247ef871a14dbd07cbcc813
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-10-02 15:01:52 +0200

    Initial commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Kuba Kociucki on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
